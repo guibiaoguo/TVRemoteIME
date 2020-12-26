75f4160dfb623edcb1dcb8fae8a9d4b7fd0b7fcc
bill <guoa1234@163.com>
2020-12-27 07:55:26 +0800

    提升了gradle版本，修复了不响应回车事件，升级了编译版本，增加了去除的类AsyncTaskCompat。增加了权限控制。修复了文件管理返回为空的问题。增加了强行编译版本的代码

--- IMEService/src/main/java/com/android/tvremoteime/IMEService.java
+++ IMEService/src/main/java/com/android/tvremoteime/IMEService.java
@@ -1,12 +1,8 @@
 package com.android.tvremoteime;
 
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.inputmethodservice.InputMethodService;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.KeyCharacterMap;
@@ -21,9 +17,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.tvremoteime.adb.AdbHelper;
 import com.android.tvremoteime.server.RemoteServer;
 import com.android.tvremoteime.server.RemoteServerFileManager;
-import com.android.tvremoteime.adb.AdbHelper;
 
 import java.io.IOException;
 
@@ -273,6 +269,8 @@ public class IMEService extends InputMethodService implements View.OnClickListen
 			i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			i.addCategory(Intent.CATEGORY_HOME);
 			this.startActivity(i);
+		} else if(keyCode == KeyEvent.KEYCODE_ENTER){
+			sendDefaultEditorAction(true);
 		}else {
 			sendDownUpKeyEvents(keyCode);
 		}
--- IMEService/src/main/java/com/android/tvremoteime/MainActivity.java
+++ IMEService/src/main/java/com/android/tvremoteime/MainActivity.java
@@ -3,27 +3,49 @@ package com.android.tvremoteime;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.tvremoteime.server.RemoteServer;
 import com.android.tvremoteime.adb.AdbHelper;
-import com.zxt.dlna.dmr.ZxtMediaRenderer;
+import com.android.tvremoteime.server.RemoteServer;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private ImageView qrCodeImage;
     private TextView addressView;
     private EditText dlnaNameText;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE" };
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        verifyStoragePermissions(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
